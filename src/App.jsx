b27ad7303ed8933e0a0c1e2981d60651a93e09bb
ymohammedsaif2005-hue <2213171036063@thenewcollege.edu.in>
2026-04-01 01:09:53 +0530

    Refactor: Revert to stable bottleneck logic and DBR scheduler

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import React, { useState } from 'react';
 import BottleneckFinder from './components/BottleneckFinder';
 import DBRScheduler from './components/DBRScheduler';
-import GlobalFinancialSettings from './components/GlobalFinancialSettings';
-import BusinessImpactDashboard from './components/BusinessImpactDashboard';
 import './App.css';
 
 function App() {
   const [bottleneck, setBottleneck] = useState(null);
-  const [financialSettings, setFinancialSettings] = useState({
-    unitSellingPrice: '',
-    unitVariableCost: '',
-    monthlyOperatingExpense: '',
-  });
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TOC Decision Support System</h1>
+        <h1>TOC Analyzer</h1>
       </header>
       <main>
-        <GlobalFinancialSettings settings={financialSettings} onSettingsChange={setFinancialSettings} />
         <BottleneckFinder setBottleneck={setBottleneck} />
         <DBRScheduler bottleneck={bottleneck} />
-        <BusinessImpactDashboard bottleneck={bottleneck} financialSettings={financialSettings} />
       </main>
     </div>
   );
